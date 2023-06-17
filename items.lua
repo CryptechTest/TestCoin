8bdac28ba2b8e72273b67b3640a975c02ebdf24a
Jon <squid@sqdmc.net>
2023-06-17 04:58:46 -0400

    update textures and items

--- items.lua
+++ items.lua
@@ -16,15 +16,67 @@ minetest.register_craftitem("testcoin:rig_part", {
 })
 
 
-minetest.register_craftitem("testcoin:pow_miner", {
+--[[minetest.register_craftitem("testcoin:pow_miner", {
     description = "PoW Miner",
     inventory_image = "testcoin_pow_miner.png",
     stack_max = 1,
+    
+})--]]
+
+minetest.register_node("testcoin:pow_miner", {
+	description = "PoW Miner",
+    stack_max = 1,
+    --inventory_image = "testcoin_pow_miner.png",
+	tiles = {
+		"testcoin_pow_miner_top.png",
+		"testcoin_pow_miner_top.png",
+		"testcoin_pow_miner_side.png",
+		"testcoin_pow_miner_side.png",
+		"testcoin_pow_miner_back.png",
+		"testcoin_pow_miner_front.png"
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	sunlight_propagates = true,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.4375, -0.5, -0.5, 0.0625, 0.5, 0.5}, -- NodeBox1
+		}
+	},
+	groups = {cracky=1, oddly_breakable_by_hand=1},
+    sounds = default.node_sound_metal_defaults(),
 })
 
-
-minetest.register_craftitem("testcoin:asic_miner", {
+--[[minetest.register_craftitem("testcoin:asic_miner", {
     description = "ASIC Miner",
     inventory_image = "testcoin_asic_miner.png",
     stack_max = 1,
+})--]]
+
+minetest.register_node("testcoin:asic_miner", {
+	description = "ASIC Miner",
+    stack_max = 1,
+    inventory_image = "testcoin_asic_miner.png",
+	tiles = {
+		"testcoin_asic_miner_top.png",
+		"testcoin_asic_miner_top.png",
+		"testcoin_asic_miner_right.png",
+		"testcoin_asic_miner_left.png",
+		"testcoin_asic_miner_back.png",
+		"testcoin_asic_miner_front.png"
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	sunlight_propagates = true,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.4375, -0.5, -0.5, 0.0625, 0.5, 0.5}, -- NodeBox1
+		}
+	},
+	groups = {cracky=1, oddly_breakable_by_hand=1},
+    sounds = default.node_sound_metal_defaults(),
 })
